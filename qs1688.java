3086564feb12fd4817473fceaa513ed6b318959d
İsmail Efe Ataş <96317271+efe-atas@users.noreply.github.com>
2023-12-05 11:50:19 +0300

    1688. Count of Matches in Tournament

---
+++ qs1688.java
@@ -0,0 +1,17 @@
+class qs1688 {
+
+    public int numberOfMatches(int n) {
+
+        if(n == 1){
+            return 0;
+        }
+
+        if(n%2 == 0)
+            return n/2 + numberOfMatches(n/2);
+        else
+            return ((n-1)/2)+numberOfMatches(((n-1)/2)+ 1);
+
+
+
+    }
+}
